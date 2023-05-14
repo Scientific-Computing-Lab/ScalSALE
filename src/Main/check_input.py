505cc1b975d6867afb955868781482baff1c87d6
yehonatan123f <anonymoussub12@gmail.com>
2023-05-14 15:58:34 +0300

    openmp branch

--- src/Main/check_input.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import argparse
-import sys
-import os
-import math
-import json
-import time
-import numpy as np
-
--- src/Scripts/run_slurm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH -n 27 -N 1 --exclusive --threads-per-core=1 -p backus  --error=slurm-%j.err --output=slurm-%j.out
-export OMP_NUM_THREADS=1
-ulimit -s unlimited
-module load intel/18.0.1.163 openmpi/4.1.3-intel
-mpirun -n 27 ../exec/main
